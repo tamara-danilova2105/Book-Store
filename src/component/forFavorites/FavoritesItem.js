b584a8339c99ec2c710d5fe4b254b77663ba329d
tamara-danilova2105 <sap1028@mail.ru>
2022-05-30 18:08:30 +0400

    изменены размеры картинок в избранном

--- src/component/forFavorites/FavoritesItem.js
+++ src/component/forFavorites/FavoritesItem.js
@@ -6,7 +6,7 @@ const FavoritesItem = ({ item }) => {
             <Card.Body>
                 <Card.Title className="card-text header-mybook">{item.name}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted card-text">{item.author}</Card.Subtitle>
-                <Card.Img className="card-image" src={`./${item.image}.jpg`} alt={item.name} />
+                <Card.Img className="favorite-image" src={`./${item.image}.jpg`} alt={item.name} />
                 <Card.Text>{item.description}</Card.Text>
                 <Button className="btn-favorites" variant="danger">УДАЛИТЬ</Button>
                 <Button className="btn-favorites" variant="outline-success">ПРОЧИТАНО</Button>
